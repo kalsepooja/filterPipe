b7498c97e21f7517a2d0ea2dcdf36e30b81dbed4
kalse pooja <kalsepooja056@gmail.com>
2024-02-07 02:18:43 +0530

    work on filter pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Iplayers } from './shared/model/players.compoent';
+import { cricketers } from './shared/const/filterArray.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filterPipe';
+  searchValue: string = '';
+  selectValue: string = '';
+  players: Iplayers[] = cricketers;
+
+  onSelect(ele:HTMLSelectElement){
+    console.log(ele.value)
+    this.selectValue = ele.value
+  }
 }
